b8e5f2252e4d451b08f929681ec99001a191b68f
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-05-29 23:14:19 +0000

    update task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template
-from flask_babel import Babel, _
+#!/usr/bin/env python3
+"""
+Parametrize templates
+"""
+
+import babel
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 babel = Babel(app)
 
-@app.route('/')
-def home():
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+     determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
     return render_template('3-index.html')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
